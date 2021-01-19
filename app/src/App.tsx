b663784b03d9d804c0a5955f7464d95f32ef6959
Edian <ediancomachio@hotmail.com>
2021-01-18 22:47:01 -0300

    refactor using hooks

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { Table, Button } from "reactstrap";
 import {
   faImage,
@@ -7,105 +7,14 @@ import {
   faMoneyCheckAlt,
   faSearchDollar,
 } from "@fortawesome/free-solid-svg-icons";
+import { Users } from "./Users";
 
-class App extends Component {
-  state = {
-    isLoading: false,
-    invoices: [{} as any],
-  };
-
-  async componentDidMount() {
-    const res = await fetch(
-      "https://lmhl5i1vuj.execute-api.us-east-1.amazonaws.com/dev/users"
-    );
-    const data = await res.json();
-    console.log(data.Items);
-    this.setState({ invoices: data.Items, isLoading: false });
-  }
-
-  remove(id: number): void {
-    let updatedInvoices = [...this.state.invoices].filter((i) => i.id !== id);
-    this.setState({ invoices: updatedInvoices });
-  }
-
-  render() {
-    const isLoading = this.state.isLoading;
-    const allInvoices = this.state.invoices;
-
-    if (isLoading) return <div>Loading...</div>;
-
-    let invoices = allInvoices.map((invoice) => {
-      return (
-        <tr key={invoice.id}>
-          <td>{invoice.id}</td>
-          <td>{invoice.name}</td>
-          <td>
-            <Button
-              className="btn btn-lg"
-              onClick={() => this.remove(invoice.id)}
-            >
-              Ok
-            </Button>
-          </td>
-          <td>
-            <Button
-              className="btn btn-lg"
-              onClick={() => this.remove(invoice.id)}
-            >
-              Nok
-            </Button>
-          </td>
-          <td>
-            <Button
-              className="btn btn-lg"
-              onClick={() => this.remove(invoice.id)}
-            >
-              50%
-            </Button>
-          </td>
-          <td>
-            <Button
-              className="btn btn-lg"
-              onClick={() => this.remove(invoice.id)}
-            >
-              ??
-            </Button>
-          </td>
-        </tr>
-      );
-    });
-
-    return (
-      <div className="container border border-secondary rouded center">
-        <div className="row">
-          <div className="col-12">
-            <h4>Loading...</h4>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-12 center text-center">
-            <Table dark responsive striped bordered hover>
-              <thead>
-                <tr>
-                  <th>Id</th>
-                  <th>User</th>
-                  <th>Title</th>
-                  <th>Complete</th>
-                </tr>
-              </thead>
-              <tbody>
-                {this.state.invoices.length === 0 ? (
-                  <td>All caught up!</td>
-                ) : (
-                  invoices
-                )}
-              </tbody>
-            </Table>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+const App: React.FC = () => {
+  return (
+    <React.StrictMode>
+      <Users />
+    </React.StrictMode>
+  );
+};
 
 export default App;
---
+++ app/src/Users.tsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from "react";
+import { Button, Table } from "reactstrap";
+
+interface User {
+  id: number
+  name: string
+}
+
+
+export const Users: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [users, setUsers] = useState<User[]>([]);
+
+  const remove = (users: User[], id: number): void => {
+    let updatedUsers = [...users].filter((i) => i.id !== id);
+    setUsers(updatedUsers);
+  }
+
+  const userTableRows = (allUsers: User[]) => {
+    return allUsers.map((user) => {
+      return (
+        <tr key={user.id}>
+          <td>{user.id}</td>
+          <td>{user.name}</td>
+          <td>
+            <Button className="btn btn-lg" onClick={() => remove(users, user.id)}>Ok</Button>
+          </td>
+          <td>
+            <Button className="btn btn-lg" onClick={() => remove(users, user.id)}>Nok</Button>
+          </td>
+          <td>
+            <Button className="btn btn-lg" onClick={() => remove(users, user.id)}>50%</Button>
+          </td>
+          <td>
+            <Button className="btn btn-lg" onClick={() => remove(users, user.id)}>??</Button>
+          </td>
+        </tr>
+      );
+    });
+  }
+
+  useEffect(() => {
+    async function getUsers() {
+      const res = await fetch("https://lmhl5i1vuj.execute-api.us-east-1.amazonaws.com/dev/users");
+      const data = await res.json();
+      setUsers(data.Items.map((u: User) => {
+        return {
+          id: u.id,
+          name: u.name
+        } as User
+      }));
+    }
+
+    getUsers();
+
+  }, []);
+
+  return (
+    <div className="container border border-secondary rouded center">
+      <div className="row">
+        <div className="col-12">
+          <h4>Loading...</h4>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 center text-center">
+          <Table dark responsive striped bordered hover>
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>User</th>
+                <th>Title</th>
+                <th>Complete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.length === 0 ? (
+                <td>All caught up!</td>
+              ) : (
+                  userTableRows(users)
+                )}
+            </tbody>
+          </Table>
+        </div>
+      </div>
+    </div>
+  )
+
+};
+
